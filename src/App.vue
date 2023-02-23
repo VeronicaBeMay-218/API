baa98c9eb8c9e5c07d52a2b4f64b88d544f09cdd
VeronicaBeMay-218 <veronica.19070007@itsmotul.edu.mx>
2023-02-22 19:57:49 -0600

    Primera configuracion

--- src/App.vue
+++ src/App.vue
@@ -5,21 +5,16 @@ import HelloWorld from "./components/HelloWorld.vue";
 
 <template>
   <header>
-    <img
-      alt="Vue logo"
-      class="logo"
-      src="@/assets/logo.svg"
-      width="125"
-      height="125"
-    />
-
+    
     <div class="wrapper">
       <HelloWorld msg="Personajes de Rick & Morty" />
 
       <nav>
+      <div class="my-7">
         <RouterLink to="/">Inicio</RouterLink>
         <RouterLink to="/personajes">Personajes</RouterLink>
         <RouterLink to="/about">Acerca de</RouterLink>
+      </div>
       </nav>
     </div>
   </header>
