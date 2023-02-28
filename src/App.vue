639969c8b4f3c2ea4e1e0846ff3c04f5dd71bf2f
VeronicaBeMay-218 <veronica.19070007@itsmotul.edu.mx>
2023-02-27 22:13:58 -0600

    imagen y cabecera

--- src/App.vue
+++ src/App.vue
@@ -2,6 +2,7 @@
 import { RouterLink, RouterView } from 'vue-router'
 import HelloWorld from './components/HelloWorld.vue'
 import Personajes from './components/Personajes.vue'
+import Cabecera from './components/Cabecera.vue'
 
 </script>
 
@@ -12,13 +13,22 @@ import Personajes from './components/Personajes.vue'
   <header>
 
     <div>
-
-
-      <nav>
-        <RouterLink to="/" class="text-yellow-500">Home</RouterLink> 
-        <RouterLink to="/about">About</RouterLink>
-        <RouterLink to="/personajes">Personajes</RouterLink>
-      </nav>
+        <nav class="p-5 shadow md:flex md:items-center md:justify-between">
+      <div class="flex justify-between items-center">
+      
+       
+      </div>
+  
+        <div class="md:flex md:items-center z-[-1] md:z-auto md:static absolute w-full left-0 md:w-auto md:py-0 py-4 md:pl-0 pl-7 md:opacity-100 opacity-0 top-[-400px] transition-all ease-in duration-500">
+        
+        <RouterLink to="/" class="bg-[#1da1f2] hover:bg-[#0c4a6e] text-white font-bold py-1 px-4 rounded-full lg:inline-block">Home</RouterLink> 
+        <RouterLink to="/about" class="bg-[#1da1f2] hover:bg-[#0c4a6e] text-white font-bold py-1 px-4 rounded-full lg:inline-block">About</RouterLink>
+        <RouterLink to="/personajes" class="bg-[#1da1f2] hover:bg-[#0c4a6e] text-white font-bold py-1 px-4 rounded-full lg:inline-block">Personajes</RouterLink>
+     
+          
+        </div>
+    </nav>
+    <Cabecera/>
     </div>
   </header>
   
